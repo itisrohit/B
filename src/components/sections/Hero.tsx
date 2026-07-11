b744c9bdc111d499230c52470437935b17a9e7a4
Rohit <dv6510@gmail.com>
2026-07-11 19:15:35 +0530

    Style Hero: resize sakura layers, reposition loading card to top-left

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -36,19 +36,19 @@ export function Hero({
       </div>
 
       {/* Sliced Sakura Branch Left - Layer 1 (Main branch body: Top Left) */}
-      <div className="absolute top-0 -left-4 z-10 w-[176px] h-[90px] sm:w-[330px] sm:h-[162px] md:w-[462px] md:h-[227px] lg:w-[594px] lg:h-[290px] pointer-events-none select-none">
+      <div className="absolute top-0 -left-4 z-10 w-[158px] h-[81px] sm:w-[297px] sm:h-[146px] md:w-[416px] md:h-[204px] lg:w-[535px] lg:h-[261px] pointer-events-none select-none">
         <Image
           src="/assets/decorations/flowers/sakura-branch-left-1.webp"
           alt="Sakura branch main left"
           fill
           priority
-          sizes="(max-width: 640px) 176px, (max-width: 768px) 330px, (max-width: 1024px) 462px, 594px"
+          sizes="(max-width: 640px) 158px, (max-width: 768px) 297px, (max-width: 1024px) 416px, 535px"
           className="object-contain object-left-top rotate-[4deg]"
         />
       </div>
 
       {/* Sliced Sakura Branch Left - Layer 2 (Single flower blossom: Below Layer 1) */}
-      <div className="absolute -left-8 z-10 top-[70px] sm:top-[130px] md:top-[185px] lg:top-[240px] w-[88px] h-[81px] sm:w-[165px] sm:h-[153px] md:w-[220px] md:h-[204px] pointer-events-none select-none">
+      <div className="absolute -left-8 z-10 top-[55px] sm:top-[105px] md:top-[150px] lg:top-[195px] w-[88px] h-[81px] sm:w-[165px] sm:h-[153px] md:w-[220px] md:h-[204px] pointer-events-none select-none">
         <Image
           src="/assets/decorations/flowers/sakura-branch-left-2.webp"
           alt="Sakura blossom cluster left"
@@ -60,7 +60,7 @@ export function Hero({
       </div>
 
       {/* Sliced Sakura Branch Left - Layer 3 (Secondary branch extension: Below Layer 2) */}
-      <div className="absolute -left-4 z-10 top-[85px] sm:top-[160px] md:top-[230px] lg:top-[285px] w-[55px] h-[20px] sm:w-[99px] sm:h-[36px] md:w-[149px] md:h-[54px] lg:w-[198px] lg:h-[72px] pointer-events-none select-none">
+      <div className="absolute -left-4 z-10 top-[70px] sm:top-[135px] md:top-[195px] lg:top-[240px] w-[55px] h-[20px] sm:w-[99px] sm:h-[36px] md:w-[149px] md:h-[54px] lg:w-[198px] lg:h-[72px] pointer-events-none select-none">
         <Image
           src="/assets/decorations/flowers/sakura-branch-left-3.webp"
           alt="Sakura branch secondary left"
@@ -68,6 +68,18 @@ export function Hero({
           priority
           sizes="(max-width: 640px) 55px, (max-width: 768px) 99px, (max-width: 1024px) 149px, 198px"
           className="object-contain object-left-top rotate-[15deg]"
+        />
+      </div>
+
+      {/* Loading Card - Top Left (reaches Sakura Layer 1) */}
+      <div className="absolute z-20 top-5 left-16 sm:top-10 sm:left-28 md:top-14 md:left-40 w-[90%] max-w-[180px] sm:max-w-[270px] md:max-w-[360px] h-[145px] sm:h-[225px] md:h-[300px] pointer-events-none select-none">
+        <Image
+          src="/assets/decorations/loading/loading-card-v1.webp"
+          alt="Welcome Loading Card"
+          fill
+          priority
+          sizes="(max-width: 640px) 90vw, (max-width: 768px) 270px, 360px"
+          className="object-contain filter drop-shadow-xl"
         />
       </div>
 
